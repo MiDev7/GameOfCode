093f9a8143dc11c3da29b9acc44de4c63cd6be2c
Hemant Mooneea <HM1133@live.mdx.ac.uk>
2024-06-20 18:15:46 +0400

    added selector to indicate level

--- game/scripts/baseLevelTemplate.js
+++ game/scripts/baseLevelTemplate.js
@@ -10,6 +10,8 @@ function loadBaseLevel(Scene)
     Scene.background.setScale(0.75);
     Scene.background = Scene.add.image(1100, 400,"border");
     Scene.background.setScale(0.75);
+    Scene.background = Scene.add.image(500,100, "currentStage")
+    Scene.background.setScale(2.5);
     Scene.button1 = Scene.add.image(960, 310,"button");
     Scene.button1.setScale(0.35);
     Scene.button2 = Scene.add.image(960, 490,"button");
@@ -32,5 +34,6 @@ function preloadBaseLevel(Scene)
     Scene.load.image("lock", "assets/images/lock.png")
     Scene.load.image("border", "assets/images/border.png")
     Scene.load.image("button", "assets/images/button.png")
+    Scene.load.image("currentStage", "assets/images/current_stage_box.png")
 }
 export {loadBaseLevel,preloadBaseLevel,loadTrueFalseLevel}
