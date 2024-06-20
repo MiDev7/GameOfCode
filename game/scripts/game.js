1e3eb09cd6b023a39e722e2e6414866a8fb7d177
Hemant Mooneea <HM1133@live.mdx.ac.uk>
2024-06-20 17:59:16 +0400

    Added game

---
+++ game/scripts/game.js
@@ -0,0 +1,17 @@
+import MenuScene  from "./menuscene.js";
+import IntroScene from "./introscene.js";
+import LevelOne from "./level1scene.js";
+window.onload = function()
+{
+    const divElement = document.getElementById('gameScreen');
+    const SCREEN_HEIGHT = divElement.clientHeight;
+    const SCREEN_WIDTH = divElement.clientWidth;
+    new Phaser.Game({
+        width: SCREEN_WIDTH, // Width of the game in pixels
+        height: SCREEN_HEIGHT, // Height of the game in  pixels
+        backgroundColor: 'black',
+        scene: [MenuScene,IntroScene,LevelOne], // The scenes of the game
+        physics: { default: 'arcade' }, // The physics engine to use
+        parent: 'gameScreen', // Create the game inside the <div id="game"> 
+    });  
+}
