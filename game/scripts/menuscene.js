2b0fa2f07810402085cc8f7d8576bdb9627e3a9b
Hemant Mooneea <HM1133@live.mdx.ac.uk>
2024-06-20 13:04:03 +0400

    Menu Scene has a press to start

--- game/scripts/menuscene.js
+++ game/scripts/menuscene.js
@@ -10,6 +10,14 @@ class MenuScene extends Phaser.Scene
     }
     create()
     {
+        this.setupMenuScene();
+    }
+    update()
+    {
+
+    }
+    setupMenuScene()
+    {
         const gameWidth = this.sys.game.config.width;
         const gameHeight = this.sys.game.config.height;
         let video = this.add.video(gameWidth/2, gameHeight/2, 'menu_background');
@@ -18,12 +26,17 @@ class MenuScene extends Phaser.Scene
         video.play(true);
 
         this.gameTitle = this.add.text(460, 100, "Code Of Mystery", { font: "100px VT323", fill: "#fff" });
-        this.gameTitle.setShadow(2, 2, "#333", 2, true, true);
+        this.gameTitle.setShadow(10, 2, "black", 2, true, true);
 
-    }
-    update()
-    {
-
+        this.gamePressToStart = this.add.text(575, 500, "Press Any Key To Start", { font: "40px VT323", fill: "#fff" });
+        this.tweens.add({
+            targets: this.gamePressToStart  ,
+            alpha: 0,
+            ease: 'Cubic.easeInOut',
+            duration: 1000,
+            yoyo: true,
+            repeat: -1
+        });
     }
 }
 export default MenuScene;
