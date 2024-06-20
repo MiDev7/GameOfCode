5e67681c6952fa6877e43f8acb84a6a68ee94d3a
Hemant Mooneea <HM1133@live.mdx.ac.uk>
2024-06-20 12:55:50 +0400

    Title Screen now has a video background

--- game/scripts/menuscene.js
+++ game/scripts/menuscene.js
@@ -6,11 +6,20 @@ class MenuScene extends Phaser.Scene
     }
     preload()
     {
-
+        this.load.video('menu_background', 'assets/videos/menu_background.mp4', 'loadeddata', false, true);
     }
     create()
     {
+        const gameWidth = this.sys.game.config.width;
+        const gameHeight = this.sys.game.config.height;
+        let video = this.add.video(gameWidth/2, gameHeight/2, 'menu_background');
+        video.setDisplaySize(gameWidth, gameHeight);
+        video.setScale(0.8);
+        video.play(true);
+
         this.gameTitle = this.add.text(460, 100, "Code Of Mystery", { font: "100px VT323", fill: "#fff" });
+        this.gameTitle.setShadow(2, 2, "#333", 2, true, true);
+
     }
     update()
     {
