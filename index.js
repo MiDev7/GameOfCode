b8a43643a01f1b6a349f332a428ea4ff81caa8c7
Taran Boodhoo <taranboodhoo@Leenas-MacBook-Air.local>
2024-06-20 12:07:52 +0400

    File Setup

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import { MongoClient, ServerApiVersion } from "mongodb";
+
+
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const password = "Taran2706!";
+const username = "tb848";
+const server = "cluster0.2ior5mc.mongodb.net";
+
+const encodedeusername = encodeURIComponent(username);
+const encodedpassword = encodeURIComponent(password);
+
+const connectionURI = `mongodb+srv://${encodedeusername}:${encodedpassword}@${server}/?retryWrites=true&w=majority&appName=Cluster0`;
+
 
 app.use(express.static("static"));
 
